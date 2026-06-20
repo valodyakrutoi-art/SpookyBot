4e6cc3e2cdc4420f0eac490bb3fef35e1c77d892
valodyakrutoi-art <valodyakrutoi@gmail.com>
2026-06-20 03:19:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 # ==================== КОНФИГУРАЦИЯ ====================
-API_ID = int(os.environ.get("API_ID", 38810606))
-API_HASH = os.environ.get("API_HASH", "ad5c6998fe3df082dfdf66f836d11b24")
-PHONE_NUMBER = os.environ.get("PHONE_NUMBER", "+16722019447")
+API_ID = int(os.environ.get("API_ID", 38389796))
+API_HASH = os.environ.get("API_HASH", "5fbfc08d29ebc68310a1ded27a6e8c79")
+PHONE_NUMBER = os.environ.get("PHONE_NUMBER", "+95 9761039794")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8690367451:AAF1dc_lnPE1z0J7AeVDUV2kcU6SUXk1Q8U")
 PORT = int(os.environ.get("PORT", 8080))
 SPOOKY_BOT = "@SpookyTimeBot"
